db1baafecc90cb67a1c4128f77e101cf5e016df2
Arve Skogvold <arve@skogvold.org>
2015-01-26 10:45:04 +0100

    Made it work in Python 2.7, and added requirements file

--- src/start_up.py
+++ src/start_up.py
@@ -3,14 +3,13 @@ Created on 26. nov. 2014
 @author: PPAR
 '''
 
-import sys
 import inclinometer_manager, output_manager
 from Phidgets import PhidgetException
 
 try:
     im = inclinometer_manager.InclinometerManager(5, 0.005, 0.5)
 except RuntimeError as e:
-    print("Error (%i) while creating the Phidget object: %s" % (e.code, e.details), file=sys.stderr)
+    print("Error (%i) while creating the Phidget object: %s" % (e.code, e.details))
     print("Exiting ...")
     exit(1)
 
@@ -18,19 +17,19 @@ try:
     im.startUpInclinometer()
     print("Inclinometer STARTED-UP.....")
 except PhidgetException as e:
-    print("Phidget Exception (%i) when trying to STARTUP the inclinometer: %s" % (e.code, e.details), file=sys.stderr)
+    print("Phidget Exception (%i) when trying to STARTUP the inclinometer: %s" % (e.code, e.details))
     exit(1)
 
 om = output_manager.OutputManager()
 
 continueLoop = True
 while continueLoop:
-    data_type = input("(h) - horizon, (m) - measurement, (q) quit> ")
+    data_type = raw_input("(h) - horizon, (m) - measurement, (q) quit> ")
     
-    if(data_type == "q"):
+    if data_type == "q":
         break
     
-    if(data_type == "h" or data_type == "m"):
+    if data_type == "h" or data_type == "m":
         
         readable_data_type = "measurement"
         if(data_type == "h"):
@@ -44,7 +43,7 @@ try:
     im.closeDownInclinometer()
     print("Inclinometer CLOSED-DOWN.....")
 except PhidgetException as e:
-    print("Phidget Exception (%i) when trying to CLOSEDOWN the inclinometer: %s" % (e.code, e.details), file=sys.stderr)
+    print("Phidget Exception (%i) when trying to CLOSEDOWN the inclinometer: %s" % (e.code, e.details))
     exit(1)
 
 exit(0)
