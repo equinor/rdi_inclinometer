7862d5042283ec3429f9091f2daee55467956d19
Arve Skogvold <arve@skogvold.org>
2015-03-05 20:12:03 +0100

    Gathered config and created status dictionary

--- mmo/__init__.py
+++ mmo/__init__.py
@@ -1,1 +1,27 @@
-from config import config
+from mmo.database import Database
+
+
+class Config(object):
+    height = 0
+
+    def __init__(self):
+        self.refresh()
+
+
+    @staticmethod
+    def get_sampling_rate():
+        """
+        8 is the lowest allowed number to use with the compass
+        """
+        return 8
+
+    def get_height(self):
+        return self.height
+
+    def refresh(self):
+        self.height = Database.get_config()['height']
+
+
+config = Config()
+
+status = {}
--- mmo/config.py
+++
@@ -1,5 +0,0 @@
-class Config(object):
-    sampling_rate = 8
-    height = 20.0
-
-config = Config()
