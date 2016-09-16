d432c69bf42a2a2c91904890f0121227d2269cb4
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2016-09-16 14:14:45 +0200

    More debug info regarding gyro

--- mmo/device/spatial.py
+++ mmo/device/spatial.py
@@ -135,24 +135,26 @@ class Spatial(SpatialLike):
         return CompassFix(*self.get_compass_raw())
 
     def get_gyro(self):
+        print("get_gyro() -> %s" % self.gyro)
         return self.gyro
 
     def get_gyro_momentary(self):
-        return {
+        gyro_momentary = {
             'gm0': self.spatial.getAngularRate(0),
             'gm1': self.spatial.getAngularRate(1),
             'gm2': self.spatial.getAngularRate(2)
         }
+        return gyro_momentary
 
     def reset_gyro(self):
-        print "Resetting gyro"
+        print("Resetting gyro")
         self.spatial.zeroGyro()
-
         self.gyro.reset()
 
     # Updates the gyro integral
     # noinspection PyUnusedLocal
     def on_spatial_data_handler(self, event):
+        print("update Gyro")
         self.gyro.update_from(self.spatial)
         # idx = self.averaging_index
         # a0, a1, a2 = self.get_gravity()
