d61852b3744ea2d4f99233669838d0f3f16d2d28
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2016-12-15 23:07:24 +0100

    adding websocket starting code to the real Flask startup script as well

--- start_actual.py
+++ start_actual.py
@@ -6,6 +6,7 @@ from mmo.device.gps import Gps
 from mmo.device.spatial import Spatial
 from mmo.device.tts import Voice
 from mmo.storage import CsvStorage, DatabaseStorage, CombinationStorage
+from mmo import web_server
 
 
 storage = CombinationStorage(DatabaseStorage(), CsvStorage('output_actual.csv'))
@@ -19,9 +20,21 @@ def say_nothing(text):
 
 binoculars = Binoculars(button=button, gps=Gps(), spatial=Spatial(), storage=storage, say=say)
 
-from mmo import web_server
 web_server.prestart(binoculars)
 app = web_server.app
+app.use_reloader = False
+app.debug = True
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, use_reloader=False, port=5000)
+    #app.run(host="0.0.0.0", debug=True, use_reloader=False, port=5000)
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+
+    try:
+        server.serve_forever()
+    except Exception as e:
+        print("Stopping fake spatial service..")
+        binoculars.spatial.stop()
+        print("Done!")
+        print(e)
