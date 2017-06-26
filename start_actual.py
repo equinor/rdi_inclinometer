300b8b55c8a9eee70029856268f23c505e401e5d
Asbjørn Alexander Fellinghaug <asbjorn@fellinghaug.com>
2017-06-26 13:54:56 +0200

    refactoring the 'real' startpoint as well.

--- start_actual.py
+++ start_actual.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 
+import logging
+from logging.handlers import RotatingFileHandler
+
+import mmo
 from mmo.binoculars import Binoculars
 from mmo.device.button import Button
 from mmo.device.gps import Gps
@@ -7,6 +11,18 @@ from mmo.device.spatial import Spatial
 from mmo.device.tts import Voice
 from mmo.storage import CsvStorage, DatabaseStorage, CombinationStorage
 from mmo import web_server
+
+app = web_server.app
+app.use_reloader = False
+app.config.from_object("config.ProductionConfig")
+logFormatter = logging.Formatter(app.config.get('LOG_FORMATTER', ''))
+
+logfile = app.config.get('LOG_FILE', None)
+handler = RotatingFileHandler(app.config.get('LOG_FILE', None), maxBytes=10000, backupCount=10)
+handler.setLevel(logging.INFO)
+handler.setFormatter(logFormatter)
+app.logger.addHandler(handler)
+mmo.logger = app.logger
 
 
 storage = CombinationStorage(DatabaseStorage(), CsvStorage('output_actual.csv'))
@@ -18,6 +34,7 @@ say = voice.say
 def say_nothing(text):
     pass
 
+
 binoculars = Binoculars(button=button,
                         gps=Gps(),
                         spatial=Spatial(),
@@ -25,14 +42,9 @@ binoculars = Binoculars(button=button,
                         say=say,
                         on_long_click=web_server.long_click_handler,
                         on_short_click=web_server.short_click_handler)
-
 web_server.prestart(binoculars)
-app = web_server.app
-app.use_reloader = False
-app.debug = True
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", debug=True, use_reloader=False, port=5000)
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
     server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
@@ -40,7 +52,7 @@ if __name__ == "__main__":
     try:
         server.serve_forever()
     except Exception as e:
-        print("Stopping fake spatial service..")
+        mmo.logger.warn("Stopping fake spatial service..")
         binoculars.spatial.stop()
-        print("Done!")
-        print(e)
+        mmo.logger.warn("Done!")
+        mmo.logger.warn(e)
